c3d69dbee3c3d8d6cf8723951f5a1dd3d9a71652
_CODE__KRATOS_ <Abhishekroy2342005@gmail.com>
2025-01-21 20:13:36 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -3,26 +3,31 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const connectDB = require('./config/database');
 const routes = require('./routes/routes');
-const cors=require('cors')
+const cors = require('cors')
 const app = express();
 
-const allowedOrigins = ['*'];
+// const allowedOrigins = ['*'];
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//       // Allow requests from the allowed origins
+//       if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//         callback(null, true); // Allow the request
+//       } else {
+//         callback(new Error('Not allowed by CORS')); // Reject the request
+//       }
+//     },
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+//     allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+//   };
+
 const corsOptions = {
-    origin: function (origin, callback) {
-      // Allow requests from the allowed origins
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        callback(null, true); // Allow the request
-      } else {
-        callback(new Error('Not allowed by CORS')); // Reject the request
-      }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
-  };
-  
-  // Enable CORS with the defined options
-  app.use(cors(corsOptions));
-  
+  origin: '*', // Allow any origin for testing/development purposes
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+};
+// Enable CORS with the defined options
+app.use(cors(corsOptions));
+
 // Middleware
 app.use(bodyParser.json());
 app.use(express.json());
